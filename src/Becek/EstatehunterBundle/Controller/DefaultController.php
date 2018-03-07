943c581d35e29ca2d3d911755791a797fd37bc6a
abecek <abcq12013@gmail.com>
2018-03-07 15:46:40 +0100

    data push before exchanging computers base components

--- src/Becek/EstatehunterBundle/Controller/DefaultController.php
+++ src/Becek/EstatehunterBundle/Controller/DefaultController.php
@@ -246,7 +246,7 @@ class DefaultController extends Controller
             $options['localization']['region'] = $this->changingPolishChars(preg_replace('<<(.*?)>>', '', $data['localizationRegion']));
             $options['addedBy'] = $data['addedBy'];
 
-/*
+            /*
             //Getting CityId, RegionId, SubRegionId and level from otodom.pl
             $responseFromOtodom = json_decode($this->otodomRequestAction($options));
             $wantedLocalizationAsArray = array_values($options['localization']);
@@ -274,7 +274,7 @@ class DefaultController extends Controller
                     }
                 }
             }
-*/
+            */
 
             $otodomGenerator = new OtodomScraper();
             $offers = $otodomGenerator->loadOffersFromPages($options);
@@ -286,6 +286,45 @@ class DefaultController extends Controller
             'data' => $data,
             'html' => $offers,
         ));
+    }
+
+    private function otodomRequest($localization) {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://www.otodom.pl/ajax/geo6/autosuggest/?data=" . $localization . "^&levels^%^5B^%^5D=REGION^&levels^%^5B^%^5D=SUBREGION^&levels^%^5B^%^5D=CITY^&levels^%^5B^%^5D=DISTRICT^&levels^%^5B^%^5D=STREET");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $headers = array();
+        //$headers[] = "Cookie: __gfp_64b=dWbRmj0wp44nADkYHJBOtezPu3d2iKXxZeVkTD..Dzr.m7; optimizelyEndUserId=oeu1508111659215r0.8812146601196635; _cmuid=7b53e6e3-4622-4f61-8203-2e2b382716a3; __gads=ID=9e9d7c034d5f80c0:T=1508111678:S=ALNI_MY76I4IvVZ9GzMVzqeaf8kVtpgS4Q; cookieBarSeen=true; optimizelySegments=^%^7B^%^222105660096^%^22^%^3A^%^22gc^%^22^%^2C^%^222106010045^%^22^%^3A^%^22false^%^22^%^2C^%^222110010055^%^22^%^3A^%^22referral^%^22^%^2C^%^222110550062^%^22^%^3A^%^22none^%^22^%^7D; _ga=GA1.2.1945782446.1508111660; fonce_current_user=1; optimizelyBuckets=^%^7B^%^7D; mobileappsbadge=1; PHPSESSID=a2lefn11b308le2petap833qk4; mobile_default=desktop; ldTd=true; fonce_current_day=1,2018-03-01; _gid=GA1.2.805843924.1519916267; newrelicInited=0; onap=15f2275b080x34aec21b-22-161e211b10ex15547513-4-1519918077; mp_5da98ecd30a0b9103c5c42f2d2c5575b_mixpanel=^%^7B^%^22distinct_id^%^22^%^3A^%^20^%^2215f2275b1e86c4-0e80b7e72ec091-3b3e5906-140000-15f2275b1e95c3^%^22^%^2C^%^22^%^24initial_referrer^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^2C^%^22^%^24initial_referring_domain^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^2C^%^22^%^24search_engine^%^22^%^3A^%^20^%^22google^%^22^%^7D";
+        $headers[] = "Accept-Encoding: gzip, deflate, br";
+        $headers[] = "Accept-Language: pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7";
+        $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36";
+        $headers[] = "Accept: application/json, text/javascript, */*; q=0.01";
+        $headers[] = "Referer: https://www.otodom.pl/";
+        $headers[] = "X-Requested-With: XMLHttpRequest";
+        $headers[] = "Connection: keep-alive";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch) . PHP_EOL;
+        }
+        curl_close ($ch);
+
+        return $result;
+    }
+
+    public function localizationAction(Request $request, $target = "otodom")
+    {
+        $localization = $request->get('localization');
+
+        if ($localization != 'null') {
+            return $this->otodomRequest($localization);
+        }
+
     }
 
     // todo: Create FormModel and function to process request
